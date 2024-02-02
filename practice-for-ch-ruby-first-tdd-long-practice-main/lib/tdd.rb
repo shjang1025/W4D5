42873466be3bdfd782ee569085d0beeff987d737
Caroline Zhang <cczhang1@yahoo.com>
2024-02-02 14:18:17 -0800

    Finish two_sum

--- practice-for-ch-ruby-first-tdd-long-practice-main/lib/tdd.rb
+++ practice-for-ch-ruby-first-tdd-long-practice-main/lib/tdd.rb
@@ -1,7 +1,24 @@
-class TDD
+class Array
+    def two_sum
+        res = []
+        self.each_with_index do |ele1, i1|
+            self.each_with_index do |ele2, i2|
+               res << [i1, i2] if i1 < i2 && ele1 + ele2 == 0
+            end
+        end
+        res 
+    end
 
 end
 
 def my_uniq(array)
-
+    res = []
+    array.each do |ele|
+        if !res.include?(ele)
+            res << ele
+        end
+    end
+    res 
 end
+
+
--- practice-for-ch-ruby-first-tdd-long-practice-main/spec/tdd_spec.rb
+++ practice-for-ch-ruby-first-tdd-long-practice-main/spec/tdd_spec.rb
@@ -7,14 +7,22 @@ describe '#my_uniq' do
       expect { my_uniq([1, 2, 1, 3, 3]) }.to_not raise_error
     end
 
-    it 'return a new array only contains unique elements' do 
-
+    it 'return a new array only contains unique elements, in the order in which they first appeared' do 
+      expect(my_uniq([1, 2, 1, 3, 3])).to eq([1, 2, 3])  # need to have no space 
     end
 
 end
 
+describe Array do
+  describe 'Array#two_sum' do
+    it 'finds all pairs of indices where the elements at those indices sum to zero' do
+      expect([-1, 0, 2, -2, 1].two_sum).to eq([[0, 4], [2, 3]])
+    end
 
-# takes in an array without error
-# return in an array only contains unique elements
-# elements should appear in order as the original array
+    it 'pairs of indices need to be in order, with the smaller index before the bigger index' do 
+      expect([-1, 0, 2, -2, 1].two_sum).to_not eq([[0, 4], [3, 2]])
+    end
+
+  end
+end
 
